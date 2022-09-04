d87623d23786af0a23c32031b10c79325f8de8ec
Afzal Ali <72311983+AfzalAli231@users.noreply.github.com>
2022-09-04 14:48:07 +0500

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -3,7 +3,8 @@ const express = require("express");
 const cors = require("cors");
 const app = express();
 const connectDb = require("./db");
-const todoRoutes = require("./routes/todo")
+const todoRoutes = require("./routes/todo");
+const path = require("path");
 
 // Use of Json
 app.use(express.json());
@@ -14,6 +15,14 @@ app.use("/", todoRoutes);
 
 // Database Connection
 connectDb();
+
+// hosting path
+
+app.use(express.static(path.join(__dirname, "../frontend/build")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "../frontend/build/index.html"));
+});
 
 // App Listener
 app.listen(process.env.PORT || 5000, () => {
