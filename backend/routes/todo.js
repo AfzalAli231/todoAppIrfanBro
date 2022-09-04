3e4b68ea06935704602a45abdec46df161fe4a36
Afzal Ali <72311983+AfzalAli231@users.noreply.github.com>
2022-09-04 15:16:08 +0500

    Update todo.js

--- backend/routes/todo.js
+++ backend/routes/todo.js
@@ -2,21 +2,50 @@ const ItemsController = require("../controllers/todo.controller");
 const router = require("express").Router();
 
 router.post("/create-item", async (req, res) => {
-    try{
+   try {
     const data = await ItemsController.createItem(req.body);
     res.status(200).json(data);
-} catch {(error) => {
-res.status(400).json({message: error.message});
-}}
+   } catch (error) {
+    
+    res.status(400).json({ message: error.message });
+   } 
 }
 );
 
-router.delete("/delete-item/:itemId", ItemsController.deleteItem);
+router.delete("/delete-item/:itemId", async (req, res) => {
+  try {
+    const data = await ItemsController.deleteItem(req.params.itemId);
+  res.status(200).json(data);
+  } catch (error) {
+    res.status(400).json({message: error.message})
+  } 
+});
 
-router.put("/edit-item/:itemId", ItemsController.editItem);
+router.put("/edit-item/:itemId", async (req, res) => {
+  try {
+    const data = await ItemsController.editItem(req.params.itemId, req.body);
+  res.status(200).json(data);
+  } catch (error) {
+    res.status(400).json({message: error.message})
+  } 
+});
 
-router.get("/get-items", ItemsController.getAllItems);
+router.get("/get-items", async (req, res) => {
+  try {
+    const data = await ItemsController.getAllItems();
+    res.status(200).json(data);
+  } catch (error) {
+    res.status(400).json({message: error.message})
+  } 
+});
 
-router.get("/get-item/:itemId", ItemsController.getSingleItem);
+router.get("/get-item/:itemId", async (req, res) => {
+  try {
+    const data = await ItemsController.getSingleItem(req.params.itemId);
+    res.status(200).json(data);
+  } catch (error) {
+    res.status(400).json({message: error.message})
+  } 
+});
 
 module.exports = router;
